823faaf5f0e29ee65e35f0bf80e78cd9c80e91d4
Bennet Montgomery <bennetlogan@gmail.com>
2019-07-09 11:38:23 -0400

    added support for naming output files

--- dividepdf.sh
+++ dividepdf.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
 
-sourcefile=$1
-
-shift 1
-
-while [ ! -z "$1" ] ;
-do
-	qpdf --pages $sourcefile $1 -- $sourcefile $1.pdf
+if [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]]
+then
+	echo "help page"
+elif [[ "$1" == "-a" ]]
+then	
 	shift 1
-done	
 
-exit
+	sourcefile=$1
+
+	shift 1
+
+	while [ ! -z "$1" ] ;
+	do
+		qpdf --pages $sourcefile $1 -- $sourcefile $2.pdf
+		shift 2
+	done
+	
+	exit
+else
+	shift 1
+
+	sourcefile=$1
+	
+	shift 1
+
+	while [ ! -z "$1" ] ;
+	do
+		qpdf --pages $sourcefile $1 -- $sourcefile $1.pdf
+		shift 1
+	done
+
+	exit
+fi
